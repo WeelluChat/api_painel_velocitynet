60a89e348b7151c4579210ab988421abe39c9bf8
bubucarx <pedassaa@gmail.com>
2025-07-19 11:06:39 -0300

    teste

--- src/models/montarCombos.js
+++ src/models/montarCombos.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Planos = require("./montarPlanos");
 
 const montarCombo = new mongoose.Schema({
-    icon: { type: Number, required: true },
+    icon: { type: Number, required: false },
     title: { type: String, required: true },
     isVisible: { type: Boolean, default: true },
     planos: [Planos.schema],
